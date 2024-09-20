6e8c0138bde74bc6cda7fb3cd341dc3b5a17e314
Sebas Escobar <sebastian.escobar@telco.com.ar>
2024-09-20 11:16:48 -0300

    uso de ws/aws-sdk-go/service/s3 (v1)

--- s3package/s3package.go
+++ s3package/s3package.go
@@ -1,52 +1,47 @@
 package s3package
 
 import (
-	"context"
 	"fmt"
 	"os"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
-// S3Client estructura para manejar el cliente de S3
-type S3Client struct {
-	client *s3.Client
-	bucket string
-	region string
+// Storage estructura para manejar el cliente de S3
+type Storage struct {
+	S3 *s3.S3
 }
 
-// NewS3Client inicializa un nuevo cliente S3
-func NewS3Client(bucket, region string) (*S3Client, error) {
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
-	if err != nil {
-		return nil, fmt.Errorf("error cargando configuración: %w", err)
+// NewS3Session inicializa una nueva sesión de S3 para MinIO o AWS S3
+func NewS3Session(awskey, awssecret, awsregion, minio_endpoint string) Storage {
+	minioSession := session.Must(session.NewSession(&aws.Config{
+		Credentials:      credentials.NewStaticCredentials(awskey, awssecret, ""),
+		Region:           aws.String(awsregion),
+		Endpoint:         aws.String(minio_endpoint),
+		DisableSSL:       aws.Bool(true),
+		S3ForcePathStyle: aws.Bool(true),
+	}))
+	return Storage{
+		S3: s3.New(minioSession),
 	}
-
-	client := s3.NewFromConfig(cfg)
-
-	return &S3Client{
-		client: client,
-		bucket: bucket,
-		region: region,
-	}, nil
 }
 
-// UploadFile sube un archivo a S3
-func (s *S3Client) UploadFile(filePath, key string) (string, error) {
+// UploadFile sube un archivo a S3 (o MinIO)
+func (s *Storage) UploadFile(filePath, key, bucket_name string) (string, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		return "", fmt.Errorf("error abriendo archivo: %w", err)
 	}
 	defer file.Close()
 
-	uploader := manager.NewUploader(s.client)
+	uploader := s3manager.NewUploaderWithClient(s.S3)
 
-	result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String(s.bucket),
+	result, err := uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucket_name),
 		Key:    aws.String(key),
 		Body:   file,
 	})
@@ -57,9 +52,9 @@ func (s *S3Client) UploadFile(filePath, key string) (string, error) {
 	return result.Location, nil
 }
 
-// DownloadFile descarga un archivo de S3
-func (s *S3Client) DownloadFile(key, downloadPath string) error {
-	downloader := manager.NewDownloader(s.client)
+// DownloadFile descarga un archivo de S3 (o MinIO)
+func (s *Storage) DownloadFile(key, downloadPath, bucket_name string) error {
+	downloader := s3manager.NewDownloaderWithClient(s.S3)
 
 	file, err := os.Create(downloadPath)
 	if err != nil {
@@ -67,8 +62,8 @@ func (s *S3Client) DownloadFile(key, downloadPath string) error {
 	}
 	defer file.Close()
 
-	_, err = downloader.Download(context.TODO(), file, &s3.GetObjectInput{
-		Bucket: aws.String(s.bucket),
+	_, err = downloader.Download(file, &s3.GetObjectInput{
+		Bucket: aws.String(bucket_name),
 		Key:    aws.String(key),
 	})
 	if err != nil {
@@ -78,10 +73,10 @@ func (s *S3Client) DownloadFile(key, downloadPath string) error {
 	return nil
 }
 
-// ListFiles lista los archivos en el bucket de S3
-func (s *S3Client) ListFiles() ([]types.Object, error) {
-	result, err := s.client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
-		Bucket: aws.String(s.bucket),
+// ListFiles lista los archivos en el bucket de S3 (o MinIO)
+func (s *Storage) ListFiles(bucket_name string) ([]*s3.Object, error) {
+	result, err := s.S3.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket_name),
 	})
 	if err != nil {
 		return nil, fmt.Errorf("error listando archivos: %w", err)
